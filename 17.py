25a289697fc73edd7f6922590797133c8a0669a6
chebup3l <130279108+chebup3l@users.noreply.github.com>
2026-08-09 01:09:54 +0900

    Add files via upload
    Signed-off-by: chebup3l <130279108+chebup3l@users.noreply.github.com>

---
+++ 17.py
@@ -0,0 +1,10 @@
+f = open("26.txt")
+n = int(f.readline())
+arr = [list(map(int, s.split())) for s in f]
+arr.sort()
+res = []
+for i in range(n-2):
+        if arr[i+1][1] - arr[i][1]-1 == 1 and arr[i+2][1] - arr[i+1][1] - 1 == 1 and \
+            arr[i][0] == arr[i+1][0] == arr[i+2][0]:
+                res.append([arr[i][0], arr[i][1] + 1])
+print(min(res))
